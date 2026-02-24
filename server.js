420fcb521bacfde60bbd8f9a636fd0d01eead163
Erik <65229112+EjayGit@users.noreply.github.com>
2026-02-24 14:37:11 +0000

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ app.listen(PORT, () => console.log(`This app is running on port ${PORT}`));
 app.post("/sendForm", async (req, res) => {
     try {
         const burnoutScore = req.body.burnoutScore;
+        const userName = req.body.userName;
         const query = await db.query(
-            `INSERT INTO burnoutRating (burnoutScore) VALUES ($1)`,
-            [burnoutScore]
+            `INSERT INTO burnoutrating (burnoutscore, username) VALUES ($1, $2)`,
+            [burnoutScore, userName]
         );
         res.json({ status: "success", values: burnoutScore });
     } catch (error) {
@@ -26,7 +27,7 @@ app.post("/sendForm", async (req, res) => {
 
 app.get('/readForm', async (req, res) => {
     try {
-        const query = await db.query(`SELECT * FROM burnoutRating ORDER BY id DESC LIMIT 20;`);
+        const query = await db.query(`SELECT * FROM burnoutrating ORDER BY id DESC LIMIT 20;`);
         res.json(query.rows);
     } catch (error) {
         console.log("db error: ", error);
@@ -34,3 +35,16 @@ app.get('/readForm', async (req, res) => {
     }
 });
 
+app.post("/sendForm", async (req, res) => {
+    try {
+        const userName = req.body.userName;
+        const password = req.body.password;
+        const query = await db.query(
+            `INSERT INTO usertable (username, password) VALUES ($1, $2)`,
+            [userName, password]
+        );
+        res.json({ status: "success", values:userName });
+    } catch (error) {
+        res.status(500).json({ Error: error.message });
+    }
+});
